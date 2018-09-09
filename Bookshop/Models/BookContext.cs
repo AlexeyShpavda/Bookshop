3b63c9be23defd07ca494694c796fe76be7efc8e
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-09-09 22:26:54 +0300

    Create an initializer to add to database

--- Bookshop/Models/BookContext.cs
+++ Bookshop/Models/BookContext.cs
@@ -12,4 +12,16 @@ namespace Bookshop.Models
 
         public DbSet<Purchase> Purchases { get; set; }
     }
+
+    public class BookDbInitializer : DropCreateDatabaseAlways<BookContext>
+    {
+        protected override void Seed(BookContext db)
+        {
+            db.Books.Add(new Book { Name = "Война и мир", Author = "Л. Толстой", Price = 220 });
+            db.Books.Add(new Book { Name = "Отцы и дети", Author = "И. Тургенев", Price = 100 });
+            db.Books.Add(new Book { Name = "Чайка", Author = "А. Чехов", Price = 150 });
+
+            base.Seed(db);
+        }
+    }
 }
